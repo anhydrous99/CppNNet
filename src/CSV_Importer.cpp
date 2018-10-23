3a86bd5ba33b49fb01af15fe5780b8b0497d8aaa
Armando Herrera <anhydrous99@gmail.com>
2018-10-23 15:38:28 -0500

    Added libcurl to project to download larger datasets

--- src/CSV_Importer.cpp
+++ src/CSV_Importer.cpp
@@ -1,5 +1,6 @@
 #include "CSV_Importer.h"
 
+#include "curl/curl.h"
 #include <iostream>
 #include <fstream>
 
@@ -31,6 +32,34 @@ void CSV_Importer::ObtainData() {
     std::cout << "Unable to open file " << _filename << std::endl;
 }
 
+/* Used by CSV_Importer to put the web source into a string type */
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+  ((std::string *) userp)->append((char *) contents, size * nmemb);
+  return size * nmemb;
+}
+
+std::string CSV_Importer::Downloader() {
+  CURL *curl;
+  CURLcode res;
+  std::string readBuffer;
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, _filename.c_str());
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl/1.0");
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if (res != CURLE_OK)
+      std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+}
+
 std::vector<std::string> CSV_Importer::GetData() {
   if (_hasdata)
     return _data;
@@ -46,7 +75,7 @@ std::vector<Eigen::VectorXf> CSV_Importer::GetSamples() {
 
   std::vector<Eigen::VectorXf> output;
   int s = _sof + _sot;
-  for (int i = 0, sizei = _data.size() / s; i < sizei; i++) {
+  for (unsigned long i = 0, sizei = _data.size() / s; i < sizei; i++) {
     Eigen::VectorXf nm(_sof);
     for (int j = 0, sizej = _sof; j < sizej; j++)
       nm[j] = std::stof(_data[i * s + j]);
@@ -61,7 +90,7 @@ std::vector<Eigen::VectorXf> CSV_Importer::GetTargets() {
 
   std::vector<Eigen::VectorXf> output;
   int s = _sof + _sot;
-  for (int i = 0, sizei = _data.size() / s; i < sizei; i++) {
+  for (unsigned long i = 0, sizei = _data.size() / s; i < sizei; i++) {
     Eigen::VectorXf nm(_sot);
     for (int j = _sof; j < s; j++)
       nm[j - _sof] = std::stof(_data[i * s + j]);
