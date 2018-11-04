c38fcb0e5389d4401a07a629f4507ece3c90182b
Armando Herrera <anhydrous99@gmail.com>
2018-11-04 02:39:11 -0600

    Trying out ReLU layer

--- tests/HIGGS.cpp
+++ tests/HIGGS.cpp
@@ -31,9 +31,8 @@ int main(int argc, char *argv[]) {
   int out = 2;
 
   // Create Layers
-  std::shared_ptr<Neural_Layer> layer1 = std::make_shared<Neural_Layer>(150, inp, activation_function::Logistic);
-  std::shared_ptr<Neural_Layer> layer2 = std::make_shared<Neural_Layer>(100, 150, layer1,
-                                                                        activation_function::Logistic);
+  std::shared_ptr<Neural_Layer> layer1 = std::make_shared<Neural_Layer>(150, inp, activation_function::ReLU);
+  std::shared_ptr<Neural_Layer> layer2 = std::make_shared<Neural_Layer>(100, 150, layer1, activation_function::ReLU);
   std::shared_ptr<Neural_Layer> layer3 = std::make_shared<Neural_Layer>(out, 100, layer2);
 
   // Import Data
@@ -47,11 +46,11 @@ int main(int argc, char *argv[]) {
   std::vector<Evector> normed_samples = samplen.get_batch_norm(samples);
 
   // Create Trainer
-  Neural_Trainer trainer(layer3, 0.0001);
+  Neural_Trainer trainer(layer3, 0.001);
 
   // Train
   std::cout << "Starting to Train\n";
-  for (int i = 0, sizei = 10; i < sizei; i++) {
+  for (int i = 0, sizei = 1; i < sizei; i++) {
     trainer.train_minibatch(normed_samples, targets, 100000);
     std::cout << "Epoch: " << i << std::endl;
   }
