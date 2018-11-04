af9adc23bc48d9ad58b8786aa28a6d464a98c788
Armando Herrera <anhydrous99@gmail.com>
2018-11-03 20:58:05 -0500

    Added so that floating point overflows, underflows, or errors throw exceptions.

--- tests/HIGGS.cpp
+++ tests/HIGGS.cpp
@@ -5,7 +5,13 @@
 #include <iostream>
 #include <chrono>
 
+// Stops training in the case of float-point overflow, underflow, or invalid.
+#ifdef __linux__
 #include <fenv.h>
+
+#else
+#pragma float_control( except, on )
+#endif
 
 
 int main(int argc, char *argv[]) {
@@ -14,18 +20,20 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  //feenableexcept(FE_INVALID | FE_OVERFLOW);
+#ifdef __linux__
+  feenableexcept(FE_INVALID | FE_OVERFLOW | FE_UNDERFLOW);
+#endif
 
   // Number of inputs
   int inp = 28;
   // Number of outputs
   int out = 2;
-  // Number of neurons
-  int nn = 400;
 
   // Create Layers
-  std::shared_ptr<Neural_Layer> layer1 = std::make_shared<Neural_Layer>(nn, inp, activation_function::Logistic);
-  std::shared_ptr<Neural_Layer> layer2 = std::make_shared<Neural_Layer>(out, nn, layer1);
+  std::shared_ptr<Neural_Layer> layer1 = std::make_shared<Neural_Layer>(150, inp, activation_function::Logistic);
+  std::shared_ptr<Neural_Layer> layer2 = std::make_shared<Neural_Layer>(100, 150, layer1,
+                                                                        activation_function::Logistic);
+  std::shared_ptr<Neural_Layer> layer3 = std::make_shared<Neural_Layer>(out, 100, layer2);
 
   // Import Data
   std::string path = argv[1];
@@ -38,17 +46,17 @@ int main(int argc, char *argv[]) {
   std::vector<Evector> normed_samples = samplen.get_batch_norm(samples);
 
   // Create Trainer
-  Neural_Trainer trainer(layer2);
+  Neural_Trainer trainer(layer3, 0.0001);
 
   // Train
   std::cout << "Starting to Train\n";
-  for (int i = 0, sizei = 100; i < sizei; i++) {
+  for (int i = 0, sizei = 10; i < sizei; i++) {
     trainer.train_minibatch(normed_samples, targets, 100000);
     std::cout << "Epoch: " << i << std::endl;
   }
 
   // Calculate Error
-  float mse = layer2->mse(normed_samples, targets);
+  float mse = layer3->mse(normed_samples, targets);
 
   std::cout << "MSE:  " << mse << std::endl;
 
